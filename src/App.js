65a9026e018fad5334653f87eb9e31c94835cbd9
stefanbpeterson <stefanbpeterson@gmail.com>
2020-03-25 00:13:45 -0600

    adding new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '0786225b392a43238dd94f3e6b586b8d'
+})
 
 const particleParams = {
   particles: {
@@ -25,17 +31,68 @@ const particleParams = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' params={particleParams} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    console.log('click')
+    app.models.predict(Clarifai.DEMOGRAPHICS_MODEL, this.state.input)
+      .then(
+        function(response) {
+          console.log(response)
+
+          console.log(response.outputs[0].data.regions[0].data.face.age_appearance.concepts[0].name)
+          console.log(response.outputs[0].data.regions[0].data.face.age_appearance.concepts[0].value)
+
+          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[0].name)
+          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[0].value)
+          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[1].name)
+          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[1].value)
+
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[0].name)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[0].value)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[1].name)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[1].value)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[2].name)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[2].value)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[3].name)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[3].value)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[4].name)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[4].value)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[5].name)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[5].value)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[6].name)
+          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[6].value)
+        },
+        function(err) {
+          console.log(err)
+        }
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={particleParams} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
-      <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures.'}
+      <p className='f4 white pt3'>
+        {'Paste an image URL and the Magic Computer Brain will try to figure out your age, gender, and ethnicity.'}
       </p>
       <div className='flex justify-center'>
         <div className='form w-60 pa5 br3 shadow-5'>
-          <input type='text' className='pa3 w-60 f4' placeholder='Enter image URL here'  />
-          <button className='pa3 w-20 grow link f4 dib'>Detect</button>
+          <input type='text' className='pa3 w-60 f4' placeholder='Enter image URL here' onChange={onInputChange} />
+          <button className='pa3 w-20 grow link f4 dib' onClick={onSubmit}>Detect</button>
         </div>
       </div>
     </div>
