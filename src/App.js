0f9d02e5abb8561e630a0d78b4d4876aa33a5f7c
stefanbpeterson <stefanbpeterson@gmail.com>
2020-03-25 10:49:42 -0600

    adding front end demographic display functionality

--- src/App.js
+++ src/App.js
@@ -36,8 +36,43 @@ class App extends Component {
     super()
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      demographics: {}
     }
+  }
+
+  demographicsData = (data) => {
+    const ageAppearance = data.outputs[0].data.regions[0].data.face.age_appearance
+    const genderAppearance = data.outputs[0].data.regions[0].data.face.gender_appearance
+    const multiculturalAppearance = data.outputs[0].data.regions[0].data.face.multicultural_appearance
+
+    return {
+      age: ageAppearance.concepts[0].name,
+      ageProbability: ageAppearance.concepts[0].value,
+      genderOne: genderAppearance.concepts[0].name,
+      genderOneProbability: genderAppearance.concepts[0].value,
+      genderTwo: genderAppearance.concepts[1].name,
+      genderTwoProbability: genderAppearance.concepts[1].value,
+      multiculturalOne: multiculturalAppearance.concepts[0].name,
+      multiculturalOneProbability: multiculturalAppearance.concepts[0].value,
+      multiculturalTwo: multiculturalAppearance.concepts[1].name,
+      multiculturalTwoProbability: multiculturalAppearance.concepts[1].value,
+      multiculturalThree: multiculturalAppearance.concepts[2].name,
+      multiculturalThreeProbability: multiculturalAppearance.concepts[2].value,
+      multiculturalFour: multiculturalAppearance.concepts[3].name,
+      multiculturalFourProbability: multiculturalAppearance.concepts[3].value,
+      multiculturalFive: multiculturalAppearance.concepts[4].name,
+      multiculturalFiveProbability: multiculturalAppearance.concepts[4].value,
+      multiculturalSix: multiculturalAppearance.concepts[5].name,
+      multiculturalSixProbability: multiculturalAppearance.concepts[5].value,
+      multiculturalSeven: multiculturalAppearance.concepts[6].name,
+      multiculturalSevenProbability: multiculturalAppearance.concepts[6].value,
+    }
+  }
+
+  displayDemographics = (demographics) => {
+    console.log(demographics)
+    this.setState({demographics: demographics})
   }
 
   onInputChange = (event) => {
@@ -48,37 +83,8 @@ class App extends Component {
     this.setState({imageUrl: this.state.input})
     console.log('click')
     app.models.predict(Clarifai.DEMOGRAPHICS_MODEL, this.state.input)
-      .then(
-        function(response) {
-          console.log(response)
-
-          console.log(response.outputs[0].data.regions[0].data.face.age_appearance.concepts[0].name)
-          console.log(response.outputs[0].data.regions[0].data.face.age_appearance.concepts[0].value)
-
-          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[0].name)
-          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[0].value)
-          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[1].name)
-          console.log(response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[1].value)
-
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[0].name)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[0].value)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[1].name)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[1].value)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[2].name)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[2].value)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[3].name)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[3].value)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[4].name)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[4].value)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[5].name)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[5].value)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[6].name)
-          console.log(response.outputs[0].data.regions[0].data.face.multicultural_appearance.concepts[6].value)
-        },
-        function(err) {
-          console.log(err)
-        }
-      );
+      .then(response => this.displayDemographics(this.demographicsData(response)))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -89,7 +95,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition demographics={this.state.demographics} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,58 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
-  return (
-    <div className='flex justify-center ma'>
-      <div className='absolute mt3'>
-        <img alt='' src={imageUrl} width='500px' height='auto' />
+const FaceRecognition = ({ imageUrl, demographics }) => {
+  if (imageUrl !== '') {
+    return (
+      <div className='flex justify-center flex-row flex-nowrap content-stretch items-center ma'>
+        <div className='mt3 flexChildren'>
+          <img alt='' src={imageUrl} width='300px' height='auto' />
+        </div>
+  
+        <div className='white flexChildren pa5 self-start'>
+          <div className='f3' id='ageAppearance'>{'Age Appearance'}</div>
+          <div className='link dim white b pointer' id='age'>{demographics.age}</div>
+          <br></br>
+          <div className='f3' id='genderAppearance'>{'Gender Appearance'}</div>
+          <div className='link dim white b pointer' id='genderOne'>{demographics.genderOne}</div>
+          <div className='link dim white b pointer' id='genderTwo'>{demographics.genderTwo}</div>
+          <br></br>
+          <div className='f3' id='multiculturalAppearance'>{'Multicultural Appearance'}</div>
+          <div className='link dim white b pointer' id='multicultural'>{demographics.multiculturalOne}</div>
+          <div className='link dim white b pointer' id='multicultural'>{demographics.multiculturalTwo}</div>
+          <div className='link dim white b pointer' id='multicultural'>{demographics.multiculturalThree}</div>
+          <div className='link dim white b pointer' id='multicultural'>{demographics.multiculturalFour}</div>
+          <div className='link dim white b pointer' id='multicultural'>{demographics.multiculturalFive}</div>
+          <div className='link dim white b pointer' id='multicultural'>{demographics.multiculturalSix}</div>
+          <div className='link dim white b pointer' id='multicultural'>{demographics.multiculturalSeven}</div>
+        </div>
+
+        <div className='white flexChildren pa5 self-start'>
+          <div className='f3' id='probability'>{'Probability'}</div>
+          <div className='link dim white b pointer' id='ageProbability'>{demographics.ageProbability}</div>
+          <br></br>
+          <div className='f3' id='probability'>{'Probability'}</div>
+          <div className='link dim white b pointer' id='genderOneProbability'>{demographics.genderOneProbability}</div>
+          <div className='link dim white b pointer' id='genderTwoProbability'>{demographics.genderTwoProbability}</div>
+          <br></br>
+          <div className='f3' id='Probability'>{'Probability'}</div>
+          <div className='link dim white b pointer' id='multiculturalProbability'>{demographics.multiculturalOneProbability}</div>
+          <div className='link dim white b pointer' id='multiculturalProbability'>{demographics.multiculturalTwoProbability}</div>
+          <div className='link dim white b pointer' id='multiculturalProbability'>{demographics.multiculturalThreeProbability}</div>
+          <div className='link dim white b pointer' id='multiculturalProbability'>{demographics.multiculturalFourProbability}</div>
+          <div className='link dim white b pointer' id='multiculturalProbability'>{demographics.multiculturalFiveProbability}</div>
+          <div className='link dim white b pointer' id='multiculturalProbability'>{demographics.multiculturalSixProbability}</div>
+          <div className='link dim white b pointer' id='multiculturalProbability'>{demographics.multiculturalSevenProbability}</div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
 
 export default FaceRecognition;
