002f94506ec063382f1c6e69a53aae1b584651d8
stefanbpeterson <stefanbpeterson@gmail.com>
2020-03-30 09:32:40 -0600

    starting ranking system additions

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,10 +4,10 @@ const Rank = () => {
   return (
     <div>
       <div className='white f3'>
-        {'Stefan, your current rank is'}
+        {``}
       </div>
       <div className='white f1'>
-        {'#1'}
+        {`I'm a bee`}
       </div>
     </div>
   )
